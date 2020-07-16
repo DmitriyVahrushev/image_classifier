b28809476253dfa4979796eb5fcedd1584d067ca
DmitriyVahrushev <notmybestmail@yandex.ru>
2020-07-16 20:21:23 +0300

    let users upload only allowed file extensions

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 
 from inference import get_prediction
 from commons import format_class_name
@@ -9,14 +9,22 @@ app = Flask(__name__)
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
+            #No file part
             return redirect(request.url)
         file = request.files.get('file')
         if not file:
             return render_template('result.html', class_name = "no file has been sent!")
+        if not allowed_file(file.filename):
+            #Not allowed file extension
+            return redirect(request.url)
         img_bytes = file.read()
         class_id, class_name = get_prediction(image_bytes=img_bytes)
         class_name = format_class_name(class_name)
